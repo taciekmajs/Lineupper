d6836af4616ed3cf5d26a2426e3987b08cd39a5c
taciekmajs <taciekmajs@gmail.com>
2025-06-23 03:55:54 +0200

    spanie

--- Lineupper.BlazorWasm/Program.cs
+++ Lineupper.BlazorWasm/Program.cs
@@ -2,11 +2,13 @@ using Blazored.LocalStorage;
 using Lineupper.BlazorWasm;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7268/") });
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
